fa2dc12d7f708d0c597d4c9896386f8f4f2eae11
wjgin <woojin0913@naver.com>
2021-11-24 17:57:04 +0900

    modified: changed icons, imgees 1. change main icon: humon picture -> astronaunt icon 2. changed chat icons: send button, disconnection button 3. added tooltip in chat icons 4. changed master icon, guest icon in chat room -> it can distinguish master from online users

--- itsme/src/main/resources/static/starboot/js/scripts.js
+++ itsme/src/main/resources/static/starboot/js/scripts.js
@@ -1,11 +1,3 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.5 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -53,6 +45,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+// contact controll
 document.querySelector('#submitButton').addEventListener('click', function (event) {
     let name = document.querySelector('#name').value;
     let email = document.querySelector('#email').value;
@@ -121,4 +114,9 @@ function contactUpdateNo() {
     document.querySelector('#message').disabled = false;
     
 }
+
+// tooltip event
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
