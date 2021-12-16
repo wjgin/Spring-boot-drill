68421d897e6218de8d17b3e78494a535db1ecc94
wjgin <woojin0913@naver.com>
2021-12-16 15:31:55 +0900

    refactor: AppConfig -> Spring Container 자바소스 configuration을 spring container로 변경 1. @Bean으로 모든 메소드 빈 생성 2. Main에서 AppConfig를 생성해 직접 사용 -> ApplicationContext(스프링 컨테이너) 생성 후 Bean 가져오기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,22 +1,41 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션 동작을 위한 구현 객체를 생성하는 역할을 한다.
+// 역할과 구현이 한 눈에 들어오게 한다.
+// 스프링 DI 컨테이너에 넣기 위해서 @Configuration을 기입
+@Configuration
 public class AppConfig {
 
+    @Bean   // 스프링 컨테이너(ApplicationContext)에 메소드 이름으로 빈 생성 (name= "") 옵션으로 이름 변경 가능, 관례상 디폴트 사용
     // 생성자 주입을 통한 구체화된 객체를 결정
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public  MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     // 오더 서비스의 구체화 설정
     public OrderService orderService(){
-        return new OrderServiceImpl(new FixDiscountPolicy(), new MemoryMemberRepository());
+        return new OrderServiceImpl(discountPolicy(), memberRepository());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
--- core/src/main/java/hello/core/member/MemberApp.java
+++ core/src/main/java/hello/core/member/MemberApp.java
@@ -1,14 +1,22 @@
 package hello.core.member;
 
 import hello.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService;
 
+/*
+        MemberService memberService;
         AppConfig appConfig = new AppConfig();
         memberService = appConfig.memberService();
+*/
+
+        // 스프링 컨테이너(ApplicationContext) 사용하기
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // (이름, 타입)의 Bean을 가져오기
 
         Member memeber = new Member(1L, "memeberA", Grade.VIP);
         memberService.join(memeber);
